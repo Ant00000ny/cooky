4cc2d3f74b62fa4f580eb3a9b01e9dd0b063b664
Mikaa <404.mikaa@gmail.com>
2023-01-05 11:05:06 +0800

    add cookie scan test for windows

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,14 +1,36 @@
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTest {
     @Test
-    public void test() {
-    
+    @EnabledOnOs(OS.WINDOWS)
+    public void test() throws IOException {
+        final Path cookieStorePath = Paths.get(SystemUtils.USER_HOME, "AppData", "Local", "Google", "Chrome");
+        try (Stream<Path> pathStream = Files.walk(cookieStorePath)) {
+            boolean cookieInfoExist = pathStream.anyMatch(p -> StringUtils.equals("Cookies", p.getFileName().toString()));
+            assertTrue(cookieInfoExist);
+        }
     }
     
     @Test
-    public void test1() {
-        System.out.println(SystemUtils.USER_DIR);
+    @EnabledOnOs(OS.MAC)
+    public void test1() throws IOException {
+        final Path cookieStorePath = Paths.get(SystemUtils.USER_HOME, "Library", "Application Support", "Google", "Chrome");
+        
+        try (Stream<Path> pathStream = Files.walk(cookieStorePath)) {
+            boolean cookieInfoExist = pathStream.anyMatch(p -> StringUtils.equals(p.toFile().getName(), "Cookies"));
+            assertTrue(cookieInfoExist);
+        }
     }
 }
